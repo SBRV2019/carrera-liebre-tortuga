4f2e053bdad6d6e2aba2ecbeaba45b6d0cd57c6b
lmiguelmh <lmiguelmh@gmail.com>
2016-05-11 15:40:21 -0500

    antes de semaforoMovimiento

--- carrera-liebre-tortuga/src/main/java/Carrera.java
+++ carrera-liebre-tortuga/src/main/java/Carrera.java
@@ -2,16 +2,99 @@ import contendiente.Animal;
 import contendiente.Liebre;
 import contendiente.Tortuga;
 
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by adun on 11/05/2016.
+ * <p>
+ * https://github.com/lmiguelmh/carrera-liebre-tortuga
+ * <p>
+ * semTortuga = Semaphore (1)
+ * Este semáforo le permite a la tortuga correr (realizar 1 movimiento). La liebre envía esta señal.
+ * semLiebre = Semaphore (1)
+ * Este semáforo le permite a la liebre correr (realizar 1 movimiento). La tortuga envía esta señal.
+ * <p>
+ * semAvanzar = Semaphore (0)
+ * Este semáforo le permite tanto a la tortuga como a la liebre seguir corriendo
+ * (realizar 1 movimiento a cada uno) después de realizarse la verificación acerca
+ * de la posición de ambos contendientes. Esta verificación se realiza después que
+ * ambos realizaron sus movimientos con el objetivo de determinar si la tortuga
+ * muerde a la liebre o de dar por finalizada la carrera. La clase principal
+ * envía esta señal.
+ * <p>
+ * semMovimiento = Semaphore (0)
+ * Este semáforo le permite a la clase principal verificar la posición de ambos
+ * contendientes para determinar si la tortuga muerde a la liebre.
+ * La liebre y la tortuga envían esta señal.
  */
 public class Carrera {
     public static void main(String[] args) throws InterruptedException {
 
+        Semaphore semaforoAvanzar = new Semaphore(0);
+        Semaphore semaforoMovimiento = new Semaphore(0);
+
         Animal liebre = new Liebre("liebre");
         Animal tortuga = new Tortuga("tortuga");
+        liebre.setSemaforoContendiente(tortuga.getSemaforoAnimal());
+        liebre.setSemaforoAvanzar(semaforoAvanzar);
+        //liebre.setSemaforoMovimiento(semaforoMovimiento);
+        tortuga.setSemaforoContendiente(liebre.getSemaforoAnimal());
+        tortuga.setSemaforoAvanzar(semaforoAvanzar);
+        //tortuga.setSemaforoMovimiento(semaforoMovimiento);
+
+        System.out.println("Bang");
+        System.out.println("Comienza la carrera");
+
+        int aleatorio;
+        aleatorio = ThreadLocalRandom.current().nextInt(1, 10 + 1);
+        liebre.setAleatorio(aleatorio);
+        tortuga.setAleatorio(aleatorio);
+
+        tortuga.start();
+        liebre.start();
+
+        while (!liebre.isMetaAlcanzada() && !tortuga.isMetaAlcanzada()) {
+            Thread.sleep(100);
+
+            semaforoAvanzar.release();
+            semaforoAvanzar.release();
+
+            aleatorio = ThreadLocalRandom.current().nextInt(1, 10 + 1);
+            liebre.setAleatorio(aleatorio);
+            tortuga.setAleatorio(aleatorio);
+        }
+
+        if (liebre.isMetaAlcanzada() && tortuga.isMetaAlcanzada()) {
+            aleatorio = ThreadLocalRandom.current().nextInt(0, 1 + 1);
+            if (aleatorio == 0) {
+                System.out.println("Es un empate!");
+            } else {
+                System.out.println("LA TORTUGA GANÓ!!! EH!!! (por más débil)");
+            }
+        } else if (liebre.isMetaAlcanzada()) {
+            System.out.println("La Liebre ganó! Ugh!");
+
+        } else if (tortuga.isMetaAlcanzada()) {
+            System.out.println("LA TORTUGA GANÓ!!! EH!!!");
+        }
+
+        // finalizamos carrera
+        liebre.setMetaAlcanzada(true);
+        tortuga.setMetaAlcanzada(true);
+        semaforoAvanzar.release();
+        semaforoAvanzar.release();
+    }
+/*
+    public static void main(String[] args) throws InterruptedException {
+
+        Semaphore semaforoAvanzar = new Semaphore(0);
+        Semaphore semaforoMovimiento = new Semaphore(0);
+
+        Animal liebre = new Liebre("liebre");
+        Animal tortuga = new Tortuga("tortuga");
+        liebre.setSemaforoContendiente(tortuga.getSemaforoAnimal());
+        tortuga.setSemaforoContendiente(liebre.getSemaforoAnimal());
 
         System.out.println("Bang");
         System.out.println("Comienza la carrera");
@@ -50,4 +133,5 @@ public class Carrera {
         tortuga.setMetaAlcanzada(true);
         tortuga.ticTac();
     }
+*/
 }
--- carrera-liebre-tortuga/src/main/java/contendiente/Animal.java
+++ carrera-liebre-tortuga/src/main/java/contendiente/Animal.java
@@ -4,16 +4,45 @@ import java.util.concurrent.Semaphore;
 
 /**
  * Created by adun on 11/05/2016.
+ * semTortuga = Semaphore (1)
+ * Este semáforo le permite a la tortuga correr (realizar 1 movimiento). La liebre envía esta señal.
+ * semLiebre = Semaphore (1)
+ * Este semáforo le permite a la liebre correr (realizar 1 movimiento). La tortuga envía esta señal.
+ * semAvanzar = Semaphore (0)
+ * Este semáforo le permite tanto a la tortuga como a la liebre seguir corriendo
+ * (realizar 1 movimiento a cada uno)
+ * después de realizarse la verificación acerca de la posición de ambos contendientes.
+ * Esta verificación se realiza
+ * después que ambos realizaron sus movimientos con el objetivo de determinar si la
+ * tortuga muerde a la liebre o de
+ * dar por finalizada la carrera. La clase principal envía esta señal.
+ * semMovimiento = Semaphore (0)
+ * Este semáforo le permite a la clase principal verificar la posición de ambos contendientes para determinar si la
+ * tortuga muerde a la liebre. La liebre y la tortuga envían esta señal.
  */
 public abstract class Animal extends Thread {
 
-    private Semaphore semaphore = new Semaphore(0);
+    private Semaphore semaforoAnimal = new Semaphore(1);
+    private Semaphore semaforoContendiente = null; //explícito!
+    private Semaphore semaforoAvanzar = null;
     private boolean metaAlcanzada = false;
     private int distancia = 1;
     private int aleatorio = 0;
 
     public Animal(String name) {
         super(name);
+    }
+
+    public Semaphore getSemaforoAnimal() {
+        return semaforoAnimal;
+    }
+
+    public void setSemaforoContendiente(Semaphore semaforoContendiente) {
+        this.semaforoContendiente = semaforoContendiente;
+    }
+
+    public void setSemaforoAvanzar(Semaphore semaforoAvanzar) {
+        this.semaforoAvanzar = semaforoAvanzar;
     }
 
     public boolean isMetaAlcanzada() {
@@ -32,10 +61,57 @@ public abstract class Animal extends Thread {
         return distancia;
     }
 
-    public void ticTac() {
-        semaphore.release();
+    //  Semaphore semaphore = new Semaphore(0);
+    //    public void ticTac() {
+    //        semaphore.release();
+    //    }
+
+    private void carrera() throws InterruptedException {
+
+        System.out.println("La " + getName() + " se encuentra en el Kilómetro " + distancia);
+
+        while (!metaAlcanzada) {
+
+            semaforoContendiente.acquire(); //espera a contendiente
+
+            int movimiento = getMovimiento(aleatorio);
+            if (movimiento == 0) {
+                //dormir 2 segundos (turnos)
+                semaforoAnimal.release();
+                //esperamos señal para avanzar turno
+                semaforoAvanzar.acquire();
+                //zzz
+                System.out.println("La " + getName() + " se encuentra en el Kilómetro " + distancia + " - Realizó el movimiento: " + getMovimientoName(aleatorio));
+                semaforoContendiente.acquire();
+            }
+
+            semaforoAnimal.release(); //ya ha hecho movimiento libero
+
+            //esperamos señal para avanzar turno
+            semaforoAvanzar.acquire();
+            {
+                distancia += movimiento;
+                if (distancia <= 1) {
+                    distancia = 1;
+                } else if (distancia >= 90) {
+                    metaAlcanzada = true;
+                }
+                System.out.println("La " + getName() + " se encuentra en el Kilómetro " + distancia + " - Realizó el movimiento: " + getMovimientoName(aleatorio));
+            }
+
+        }
     }
 
+    @Override
+    public void run() {
+        try {
+            carrera();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+/*
     @Override
     public void run() {
 
@@ -59,7 +135,7 @@ public abstract class Animal extends Thread {
                 }
                 System.out.println("La " + getName() + " se encuentra en el Kilómetro " + distancia + " - Realizó el movimiento: " + getMovimientoName(aleatorio));
             } else {
-                //dormir 1 vez, en el sgte se despiera
+                //dormir 1 vez, en el sgte se despierta
                 try {
                     semaphore.acquire();
                 } catch (InterruptedException e) {
@@ -68,6 +144,7 @@ public abstract class Animal extends Thread {
             }
         }
     }
+*/
 
     public abstract int getMovimiento(int random);
 
